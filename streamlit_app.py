1cdcfb9ffd7ca58181b820a9747ec9a20c1f0c07
Tillo <tillo.bosshart@gmail.com>
2024-12-13 13:37:37 +0000

    initial

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,32 @@
 import streamlit as st
+import requests
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+st.title("Currency converter")
+
+@st.cache_data
+def get_currencies():
+    response = requests.get('https://api.frankfurter.dev/v1/latest').json()
+    return list(response['rates'].keys())
+
+currencies = get_currencies()
+
+amount = st.sidebar.number_input("Amount")
+source_currency = st.sidebar.selectbox("Source currency",currencies)
+target_curency = st.sidebar.multiselect("Target curency",currencies)
+d = st.sidebar.date_input("Date")
+
+calculate = st.sidebar.button("Convert", use_container_width=True)
+
+if calculate:
+    url = f"https://api.frankfurter.dev/v1/{d}?amount={amount}&from={source_currency}&to={','.join(target_curency)}"
+    st.write(f"URL: {url}")
+
+    response = requests.get(url)
+    json = response.json()
+
+    # st.write(json)
+
+
+    for c in json['rates']:
+        st.write(f"{json['rates'][c]} {c}")
